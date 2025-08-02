cfede489186bee9e882c589e251b962c79459ef2
Rençber AKMAN <184839345+RENCBERAKMAN@users.noreply.github.com>
2025-08-02 12:20:30 +0300

    Add javacode folder

---
+++ javacode/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Merhaba RENCBER");//println bu satırdan sonra yazilacak kodu alt satıdan yazarak devam eder.
+      System.out.println("Ben rencber akman " + //uzun paragraflarda çok yana uzamasın diye artı ekleyip alt satıra alabiliriz.
+              "su an bigisayar programcılıgı " +
+              "2. sinif ogrencisisiyim");
+      System.out.print("rencber");//print alt satıra geçmeden bir sonraki kodu yanına yazdirir.
+        System.out.println(12 + "12");//cift tirnak icindeki metinsel bir ifade oldugu icin toplanmaz.
+        System.out.println(12 + 12);//iki sayisal deger olursa artı operatoru devreye girer ve toplar.
+        System.out.print("BACK\n");//sondaki \n=println yani oda bir sonraki satira alt satirda baslar.
+        System.out.println("END");
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("BACK\tEND");//\t bir tab bosluk birakir.
+
+        }
+    }
