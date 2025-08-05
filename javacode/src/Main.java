b164b988ed6a1ef34425e0267d6e64af936eeb79
Rençber AKMAN <184839345+RENCBERAKMAN@users.noreply.github.com>
2025-08-05 09:03:27 +0300

    Javacodelearning

--- javacode/src/Main.java
+++ javacode/src/Main.java
@@ -1,39 +1,26 @@
 import javax.swing.plaf.synth.SynthDesktopIconUI;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Merhaba RENCBER");//println bu satırdan sonra yazilacak kodu alt satıdan yazarak devam eder.
-      System.out.println("Ben rencber akman " + //uzun paragraflarda çok yana uzamasın diye artı ekleyip alt satıra alabiliriz.
-              "su an bigisayar programcılıgı " +
-              "2. sinif ogrencisisiyim");
-      System.out.print("rencber");//print alt satıra geçmeden bir sonraki kodu yanına yazdirir.
-        System.out.println(12 + "12");//cift tirnak icindeki metinsel bir ifade oldugu icin toplanmaz.
-        System.out.println(12 + 12);//iki sayisal deger olursa artı operatoru devreye girer ve toplar.
-        System.out.print("BACK\n");//sondaki \n=println yani oda bir sonraki satira alt satirda baslar.
-        System.out.println("END");
-        System.out.println("BACK\tEND");//\t bir tab bosluk birakir.
+   int odev,vize,finall;
+   double ort;
+   Scanner bilgiler = new Scanner(System.in);
+   System.out.println("Odev notunu giriniz :");odev = bilgiler.nextInt();
+   System.out.println("Vize notunu giriniz :");vize = bilgiler.nextInt();
+   System.out.println("Final notunu giriniz:");finall = bilgiler.nextInt();
+   ort = (odev*0.20)+(vize*0.30)+(finall*0.50);
+   if (ort <=60){
+     System.out.println(ort + ":Ortalama ile kaldiniz");
+   }
+   else {
+     System.out.println(ort +":Ortalama ile gectiniz :");
+   }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      byte rencber = 100;
-      System.out.println("rencberin degeri :" +rencber);
-      short akman = 1000;
-      System.out.println("akmanin degeri :"+akman);
-      int deneme = 30000;
-      System.out.println("deneme degeri :"+deneme);
-      long deneme1 = 23234444;
-      System.out.println("deneme1 degeri :"+deneme1);
-      float renc = 134.9f;
-      System.out.println("Float degeri :"+ renc);
-      double akm = 3.05;
-      System.out.println("Double degeri :"+akm);
-      char u = 112;
-      System.out.println("Char :"+u);
-      String yazi = "Rencber akman";
-      System.out.println("String :"+yazi);
-      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                               //|| : Veya                            //% : Mod alma        //a++ :Artırma
                              // != :Eşit değil                      //== : Eşitlik        //b-- :Eksiltme
                             //!(a&&b) :Değil                       //&& : ve
-      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         }
     }
